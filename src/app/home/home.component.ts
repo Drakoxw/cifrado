cd988908d2ff4c570c319336b291cfb8562e71a9
Drako <dragon12xw@gmail.com>
2021-04-20 03:05:24 -0500

    feat - animations

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+
+import { Lexer } from '@angular/compiler';
+import { Component, ElementRef, HostListener, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
@@ -6,10 +8,40 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  y:any = 'non'
+  x:any = 'none'
+  www:any;
 
-  constructor() { }
+  @HostListener('window:scroll', [])
+
+  scroll():void {
+    const rect = this.el.nativeElement.getBoundingClientRect();
+    let q = ((rect.top / 4)+(rect.top * 5));
+    console.log('q',q);
+
+    if (rect.top < 500) {
+      this.x = ''
+      this.y = ''
+    }
+    if (rect.top > 501 && rect.top < (q)) {
+      this.x = 'activx'
+      this.y = 'activy'
+    }
+
+    console.log('top', rect.top);
+  }
+
+  private mapRange(a1:number,a2:number, b1:number, b2:number,value:number ){
+    return b1 + ((value - a1) * (b2 -b1))/ (a2 -a1);
+  }
+
+  constructor(private el: ElementRef) {
+    this.www = this.scroll
+  }
 
   ngOnInit(): void {
+
+
   }
 
 }
